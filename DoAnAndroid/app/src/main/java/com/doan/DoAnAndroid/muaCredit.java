15f5c26924cc028be8c1ad4b70c271eb87d4d954
lamtruongxxy <55741716+lamtruongxxy@users.noreply.github.com>
2019-12-31 17:23:35 +0700

    v8.1

---
+++ DoAnAndroid/app/src/main/java/com/doan/DoAnAndroid/muaCredit.java
@@ -0,0 +1,102 @@
+package com.doan.DoAnAndroid;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class muaCredit extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
+
+    private RecyclerView rcvGoiCredit;
+    private ArrayList<Credit> mLstCredit;
+    private CreditAdapter adapter;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+
+    private String token;
+
+    private final static String FILE_NAME_SHAREREF = "com.doan.DoAnAndroid";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_muacredit);
+
+        this.mLstCredit = new ArrayList<>();
+        this.rcvGoiCredit = this.findViewById(R.id.rcvGoiCredit);
+        this.adapter = new CreditAdapter(this, this.mLstCredit);
+        this.rcvGoiCredit.setAdapter(this.adapter);
+        this.rcvGoiCredit.setLayoutManager(new GridLayoutManager(this, 2));
+        this.rcvGoiCredit.addItemDecoration(new CreditGD(2, 64, true));
+
+        sharedPreferences = getSharedPreferences(FILE_NAME_SHAREREF, MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        token = sharedPreferences.getString("TOKEN", "");
+
+        if (token == "") {
+            finish();
+        }
+        if (getSupportLoaderManager().getLoader(0) != null) {
+            getSupportLoaderManager().initLoader(0, null, this);
+        }
+        getSupportLoaderManager().restartLoader(0, null, this);
+    }
+    @Override
+    public void finish() {
+        super.finish();
+        //overridePendingTransition(R.anim.slide_left,R.anim.sile_out_right);
+    }
+
+//    public void launchActivityProfile(View view) {
+
+    //    public void launchActivityProfile(View view) {
+
+//        startActivity(new Intent(CreditActivity.this,ProfileActivity.class));
+//        overridePendingTransition(R.anim.sile_right,R.anim.sile_out_left);
+//    }
+    @NonNull
+    @Override
+    public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
+        return new CreditLoader(this,token);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String data) {
+        Log.d("creditTest", data); //test
+        try {
+            JSONObject obj = new JSONObject(data);
+            JSONArray items = obj.getJSONArray("data");
+            for (int i=0; i<items.length(); i++) {
+                JSONObject item = items.getJSONObject(i);
+                int id = item.getInt("id");
+                String tenGoiCredit = item.getString("ten_goi");
+                int soTien = item.getInt("so_tien");
+                int soCredit = item.getInt("credit");
+
+                this.mLstCredit.add(new Credit(id, tenGoiCredit, soTien, soCredit));
+            }
+            this.adapter.notifyDataSetChanged();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+
+    }
+}
---
+++ DoAnAndroid/app/src/main/java/com/doan/DoAnAndroid/quanLyTaiKhoan.java
@@ -0,0 +1,18 @@
+package com.doan.DoAnAndroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+
+public class quanLyTaiKhoan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quanlytaikhoan);
+    }
+}
